c804f16c65e21e1acd07577b0e2bd5a35a41d5e9
jayylmao <jaymichael_carlos@icloud.com>
2024-02-23 22:34:31 +0800

    feat: add passenger embark info input

--- display.h
+++ display.h
@@ -1,24 +1,58 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "passenger.h"
+#include "file.h"
 
 #define NONE '\0'
 #define PASSENGER '1'
 #define PERSONNEL '2'
 #define EXIT '3'
 
+/*
+ *	passengerRoutine allows a passenger to select a trip to embark on.
+ */
 void passengerRoutine()
 {
+	FILE * fp = NULL;
+
 	Passenger passenger;
-	printf("Enter your trip number: ");
+	printf("Enter your trip number: AE");
 	scanf("%d", &passenger.tripNumber);
+
+	printf("Priority number list\n\
+\t1: Faculty & ASF with Inter-campus assignments\n\
+\t2: Students with Inter-campus enrolled subjects/thesis\n\
+\t3: Researchers\n\
+\t4: School Administrators\n\
+\t5: University Fellows\n\
+\t6: Employees and Students with official business\n\
+Falsifying priority is punishable by death.\n\
+Enter your priority number: ");
+	scanf("%d", &passenger.priorityNumber);
+	getchar(); // removing this causes the next input to get dropped.
+
+	printf("Enter your name: ");
+	fgets(passenger.name, MAX, stdin);
+
+	printf("Enter your ID: ");
+	fgets(passenger.id, MAX, stdin);
+
+	printf("Enter your drop-off point: ");
+	scanf("%d", &passenger.dropOffPt);
+
+	writeTripData(fp, passenger);
 }
 
+/*
+ *	personnelRoutine allows personnel to view the passenger database.
+ */
 void personnelRoutine()
 {
 	printf("Personnel Management Console\n");
 }
 
+/*
+ *	mainMenu allows the user to choose between the program's main routines.
+ *	The other routines should return to this one.
+ */
 void mainMenu()
 {
 	char userChoice = NONE;
@@ -30,7 +64,7 @@ void mainMenu()
 		printf("2. Personnel \n");
 		printf("3. Exit \n");
 
-		scanf("%c", &userChoice);
+		scanf(" %c", &userChoice);
 		system("clear||cls");
 
 		switch (userChoice) {
@@ -39,6 +73,8 @@ void mainMenu()
 			break;
 		case PERSONNEL:
 			personnelRoutine();
+			break;
+		case EXIT:
 			break;
 		default:
 			printf("Please input a number from 1 - 3.\n");
--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <stdbool.h>
 #include "display.h"
 
 int main()
