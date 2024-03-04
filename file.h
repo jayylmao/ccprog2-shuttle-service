d0eb10825e319f224a7099f56f00114c6da01cf4
jayylmao <jaymichael_carlos@icloud.com>
2024-03-04 08:03:48 +0800

    refactor: add function to convert lines from file to structs

--- file.h
+++ file.h
@@ -1,3 +1,12 @@
+#define TRIP_NUMBER 0
+#define EMBARK_PT 1
+#define PASSENGER_NAME 2
+#define ID_NUMBER 3
+#define PRIORITY_NUMBER 4
+#define DATE_OF_TRIP 5
+#define TIME_OF_TRIP 6
+#define DROP_OFF_PT 7
+
 /*
  *  setEmbarkName uses a trip number to write the embark point for that trip.
  *  Precondition: Valid trip number provided and destination string has valid length.
@@ -22,13 +31,52 @@ void setEmbarkName(int tripNumber, char embarkName[])
  *  @param fp pointer to schedule CSV file to read from.
  *  @return departure time in integer.
  */
-void getTripTime(int tripNumber, Bus *bus, FILE *fp) {
-    // int time = 0;
+int getTripTime(int tripNumber, Bus *bus, FILE *fp)
+{
+    int time = 0;
+    
 
-    return 0;
+    return time;
 }
 
+/*
+ *  readFileLine() reads a line from the buffer and saves it to the appropriate
+ *  part of the nth Passenger in a bus depending on what line is being read at a given time.
+ *  @param *buffer pointer to buffer string to read from.
+ *  @param *tripName trip name determines if 
+ */
+void readFileLine(char *buffer, char *tripName, Bus *output, bool passengerInTrip, int currentLine, Date date)
+{
+    int passengerCount;
+    passengerCount = output->passengerCount;
 
+    if (passengerInTrip) {
+        switch (currentLine)
+        {
+        case TRIP_NUMBER:
+            output->passengers[passengerCount].tripNumber = atoi(buffer);
+            passengerCount++;
+            break;
+        case EMBARK_PT:
+            // output->passengers[passengerCount]; still deciding how embark point works
+            passengerCount++;
+            break;
+        case PASSENGER_NAME:
+            strcpy(output->passengers[passengerCount].name, buffer);
+            break;
+        case ID_NUMBER:
+            strcpy(output->passengers[passengerCount].id, buffer);
+            break;
+        case PRIORITY_NUMBER:
+            output->passengers[passengerCount].priorityNumber = atoi(buffer);
+            break;
+        case DATE_OF_TRIP:
+            break;
+        default:
+            break;
+        }
+    }
+}
 
 /*
  *  TODO: readTripData reads a specified trip number from a given file and outputs all passenger data to a Bus struct
@@ -49,12 +97,15 @@ Bus readTripData(Date date, int tripNumber) // will return a Bus struct when don
 
     char buffer[MAX];
     int lineCount = 0;
+    int currentLineOfRecord;
+
+    // check if the passenger from the file being read is in the trip
+    // based on the trip name in the record.
+    bool passengerInTrip = false;
     
     // append trip number to "AE" for comparison with trip name in file.
     char tripName[6] = "AE";
     snprintf(tripName, sizeof(tripName), "AE%d", tripNumber);
-
-    int currentLine;
 
     Bus output;
     output.passengerCount = 0;
@@ -67,12 +118,20 @@ Bus readTripData(Date date, int tripNumber) // will return a Bus struct when don
         while (fgets(buffer, MAX, inputFile) != NULL) {
             // all passenger profiles in the trip file are 8 lines long. 
             // (7 because computer counting starts from 0)
-            currentLine = lineCount % 7;
-            if (currentLine == 0 && strcmp(buffer, tripName) == 0) {
-                output.passengers[output.passengerCount].tripNumber = atoi(buffer);
-                output.passengerCount++;
+            currentLineOfRecord = lineCount % 7;
+
+            // if we're reading the trip number line, check if that's the trip
+            // number we're searching for. if so, read the next lines until
+            // we reach the next trip number line.
+            if (currentLineOfRecord == TRIP_NUMBER) {
+                if (strcmp(buffer, tripName) == 0) {
+                    passengerInTrip = true;
+                } else {
+                    passengerInTrip = false;
+                }
             }
 
+            readFileLine(buffer, tripName, &output, passengerInTrip, currentLineOfRecord, date);
             lineCount++;
         }
     }
