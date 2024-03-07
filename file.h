17b97d8538577e52342b9dcb3c330142c03cb0f8
jayylmao <jaymichael_carlos@icloud.com>
2024-03-07 09:53:02 +0800

    refactor: change output file format

--- file.h
+++ file.h
@@ -8,42 +8,23 @@
 #define DROP_OFF_PT 7
 
 /*
- *  setEmbarkName uses a trip number to write the embark point for that trip.
- *  Precondition: Valid trip number provided and destination string has valid length.
- *  @param tripNumber trip number.
- *  @param embarkName pointer to string to save embark point name to.
- */
-void setEmbarkName(int tripNumber, char embarkName[])
-{
-    if (tripNumber >= 101 && tripNumber <= 109) {
-        strcpy(embarkName, "South Gate Driveway (Manila Campus)");
-    } else if (tripNumber >= 150 && tripNumber <= 160) {
-        strcpy(embarkName, "Milagros del Rosario Building - East Canopy (Laguna Campus)");
-    } else {
-        strcpy(embarkName, "Unknown trip number.");
-    }
-}
-
-/*
  *  getTripTime gets the trip time from the schedule CSV file.
  *  Precondition: tripSched.csv file exists in a valid format.
- *  @param tripNumber trip number.
- *  @param fp pointer to schedule CSV file to read from.
- *  @return departure time in integer.
+ *  @param tripNumber Trip number.
+ *  @param fp Pointer to schedule CSV file to read from.
+ *  @return Departure time in integer.
  */
 int getTripTime(int tripNumber, Bus *bus, FILE *fp)
 {
     int time = 0;
-    
-
     return time;
 }
 
 /*
  *  readFileLine() reads a line from the buffer and saves it to the appropriate
  *  part of the nth Passenger in a bus depending on what line is being read at a given time.
- *  @param *buffer pointer to buffer string to read from.
- *  @param *tripName trip name determines if 
+ *  @param *buffer Pointer to buffer string to read from.
+ *  @param *tripName Trip name determines if 
  */
 void readFileLine(char *buffer, char *tripName, Bus *output, bool passengerInTrip, int currentLine, Date date)
 {
@@ -82,9 +63,9 @@ void readFileLine(char *buffer, char *tripName, Bus *output, bool passengerInTri
  *  TODO: readTripData reads a specified trip number from a given file and outputs all passenger data to a Bus struct
  *  for access within the program.
  *  @param date Current program run date to determine filename from.
- *  @return output Bus struct.
+ *  @return None.
  */
-Bus readTripData(Date date, int tripNumber) // will return a Bus struct when done.
+void readTripData(Date date, int tripNumber, Bus *output)
 {
     // create file name string from given date.
     char fileName[MAX];
@@ -96,7 +77,8 @@ Bus readTripData(Date date, int tripNumber) // will return a Bus struct when don
     inputFile = fopen(fileName, "r");
 
     char buffer[MAX];
-    int lineCount = 0;
+    int lineCount;
+    lineCount = 0;
     int currentLineOfRecord;
 
     // check if the passenger from the file being read is in the trip
@@ -107,8 +89,7 @@ Bus readTripData(Date date, int tripNumber) // will return a Bus struct when don
     char tripName[6] = "AE";
     snprintf(tripName, sizeof(tripName), "AE%d", tripNumber);
 
-    Bus output;
-    output.passengerCount = 0;
+    output->passengerCount = 0;
 
     // check for existence of file.
     if (inputFile == NULL) {
@@ -131,91 +112,83 @@ Bus readTripData(Date date, int tripNumber) // will return a Bus struct when don
                 }
             }
 
-            readFileLine(buffer, tripName, &output, passengerInTrip, currentLineOfRecord, date);
+            readFileLine(buffer, tripName, output, passengerInTrip, currentLineOfRecord, date);
             lineCount++;
         }
     }
 
     fclose(inputFile);
-
-    return output;
 }
 
 /*
- *	writeTripData checks for the existence of a file to write trip data to and
- *	creates it if it does not exist.
- *	Precondition: Valid name, route, ID, and number are provided.
- *	@param fp pointer to a file to write to.
- *  @param date date struct for 
- *	@return None.
+ *  writeFile takes all the buses in the array and writes all passenger and trip info
+ *  to a file specified by the date.
+ *  @param *buses Pointer to array of buses whose info will be written to the file.
+ *  @param nBuses Number of buses in the array.
+ *  @param date Date structure to determine file name.
+ *  @return None.
  */
-void writeTripData(FILE *fp, Passenger passenger, Date date)
+void writeFile(Bus *buses, int nBuses, Date date)
 {
-    // struct passenger user;
-    // int valid = 0;
+    FILE *fp;
 
-    // Im not yet ready to use this function
-    // int tripNum = getTripNum(*folderPath);
-
-    /*
-        This is to form the file name (I haven't added the Bus number function yet)
-        It should be Trip-dd-mm-yyyy.txt, for now it's only dd-mm-yyyy.txt
-    */ 
-
-    /* 
-        This is to combine the path and the text file together into onc string.
-        It's not strcat() because folderPath is a pointer.
-    */
-    char destPath[MAX];
-    snprintf(destPath, sizeof(destPath), "./trips/%02d-%02d-%04d.txt", date.date, date.month, date.year);
-
-    // this is for file reading/writing/appending
-    fp = fopen(destPath, "a");
-
-    // If file does not exist, make new file with current date.
-    if (fp == NULL) {
-        fp = fopen(destPath, "w");
-        if (fp != NULL) {
-            // File created successfully, you can now write to it or do other operations
-            fp = fopen(destPath, "a");
-        } else {
-            printf("Error: Could not create file %02d-%02d-%04d.txt at directory ./trips/.\n", date.date, date.month, date.year);
-        }
-    }
+    int i, j;
+    int passengerCount;
+    Passenger passenger;
 
     char embarkPoint[60];
 
-    setEmbarkName(passenger.tripNumber, embarkPoint);
+	char destPath[MAX];
+    snprintf(destPath, sizeof(destPath), "./trips/%02d-%02d-%04d.txt", date.date, date.month, date.year);
 
-    // write passenger data to file.
-    fprintf(fp, "AE%d\n", passenger.tripNumber);
-    fprintf(fp, "%s\n", embarkPoint);
-    fprintf(fp, "%s\n", passenger.name);
-    fprintf(fp, "%s\n", passenger.id);
-    fprintf(fp, "%d\n", passenger.priorityNumber);
-    fprintf(fp, "%02d-%02d-%04d\n", date.date, date.month, date.year);
-    fprintf(fp, "%02d:%02d\n", 6, 00); // hardcoded for now. still have to figure out time.
-    fprintf(fp, "%d\n", passenger.dropOffPt);
-    fprintf(fp, ".\n.\n.\n");
+	// this is for file reading/writing/appending
+    fp = fopen(destPath, "w");
+
+    // error if file could not be created.
+    if (fp == NULL) {
+        printf("Error: Could not create file %02d-%02d-%04d.txt at directory ./trips/.\n",
+		date.date, date.month, date.year);
+    }
+
+    // iterate through each bus.
+    for (i = 0; i < nBuses; i++) {
+        passengerCount = buses[i].passengerCount;
+        
+        if (passengerCount > 0) {
+            fprintf(fp, "AE%d\n", buses[i].tripNumber);
+        }
+
+        for (j = 0; j < passengerCount; j++) {
+            passenger = buses[i].passengers[j];
+            setEmbarkName(passenger.tripNumber, embarkPoint);
+
+            // write passenger data to file.
+            fprintf(fp, "%s\n", embarkPoint);
+            fprintf(fp, "%s\n", passenger.name);
+            fprintf(fp, "%s\n", passenger.id);
+            fprintf(fp, "%d\n", passenger.priorityNumber);
+            
+            // this only gets prints for 1st passenger as per MP specs.
+            if (j == 0) {
+                fprintf(fp, "%02d-%02d-%04d\n", date.date, date.month, date.year);
+            }
+            
+            fprintf(fp, "%02d:%02d\n", 6, 00); // hardcoded for now. still have to figure out time.
+            fprintf(fp, "%d\n", passenger.dropOffPt);
+            
+            // add extra newline for all other passengers as per MP specs.
+            if (j != 0) {
+                fprintf(fp, "\n");
+            }
+
+            fprintf(fp, ".\n.\n.\n");
+        }
+    }
 
     fclose(fp);
 }
 
-/*
- *	getPassenger parses a file and returns a passenger struct.
- *	Precondition: File is formatted properly.
- *	@param fp file to read data from.
- *	@return struct containing all passenger info.
-
-Passenger getPassenger(FILE *fp)
+void readFile(FILE *fp, Bus *buses, int n, Date date)
 {
-	Passenger passenger;
 
-	const char *folderPath = "/.trips/";
 }
-
-Bus getBus(FILE *fp)
-{
-	Bus bus;
-	return bus;
-}*/
