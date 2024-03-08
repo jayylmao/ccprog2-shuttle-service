7cec5bf432a15aabfaf174aae30e0dee4e5c1ccf
jayylmao <jaymichael_carlos@icloud.com>
2024-03-08 13:14:53 +0800

    feat: add personnel functions

--- personnel.h
+++ personnel.h
@@ -1,4 +1,4 @@
-#define VIEW_PASSENGERS_ON_TRIP '1'
+#define VIEW_PASSENGERS_COUNT '1'
 #define VIEW_PASSENGERS_AT_DROP '2'
 #define VIEW_PASSENGER_INFO '3'
 #define LOAD_PASSENGER_INFO '4'
@@ -6,128 +6,108 @@
 #define LOAD_FILE '6'
 #define PERSONNEL_EXIT '7'
 
-void viewPassengersOnTrip()
+/*
+ *	viewPassCount shows the number of passengers on a specified trip.
+ *	Precondition: trips[] array exists.
+ *	@param trips[] Array containing all trips in memory.
+ *	@param nTrips Number of trips in the array.
+ *	@return None.
+ */
+void viewPassCount(Trip trips[], int nTrips)
+{
+	int input, i;
+	printf("Enter a trip number: AE");
+	scanf("%d", &input);
+
+	// iterate through each trip in the array.
+	for (i = 0; i < nTrips; i++) {
+		if (trips[i].tripNumber == input) {
+			printf("Number of passengers on trip AE%d: %d\n", trips[i].tripNumber, trips[i].passengerCount);
+		}
+	}
+}
+
+/*
+ *	viewPassAtDrop shows the number of passengers at each drop off point.
+ *	Precondition: trips[] array exists.
+ *	@param trips[] Array containing all trips in memory.
+ *	@param nTrips Number of trips in the array.
+ *	@return None.
+ */
+void viewPassAtDrop(Trip trips[], int nTrips)
+{
+	int input, i, j;
+	printf("Enter a trip number: AE");
+	scanf("%d", &input);
+
+	Trip trip;
+	Passenger passenger;
+
+	for (i = 0; i < nTrips; i++) {
+		trip = trips[i];
+
+		for (j = 0; j < trip.passengerCount; j++) {
+			passenger = trip.passengers[j];
+			// TODO: finish this function. this will be removed and is just to avoid the warning.
+			printf("%s", passenger.Name.firstName);
+		}
+	}
+}
+
+void viewPassInfo(Trip trips[], int nTrips)
 {
 
 }
 
-void viewPassengersAtDrop()
+void loadPassInfo()
 {
 
 }
 
-void viewPasssengerInfo()
+void searchPass(Trip trips[], int nTrips)
 {
+	int i, j, passengersFound = 0;
+	char input[MAX];
 
-}
+	Passenger passenger;
 
-void loadPassengerInfo()
-{
+	Passenger output[MAX];
 
-}
+	system("clear||cls");
 
-void searchPassenger()
-{
+	do {
+		printf("Passenger search\n");
+		printf("Enter the last name of the passenger to search for, or type 0 to return: ");
+		scanf("%s", input);
 
+		for (i = 0; i < nTrips; i++) {
+			for (j = 0; j < trips[i].passengerCount; j++) {
+				passenger = trips[i].passengers[j];
+				
+				if (strcmp(passenger.Name.lastName, input) == 0) {
+					output[passengersFound] = passenger;
+					passengersFound++;
+				}
+			}
+		}
+
+		for (i = 0; i < passengersFound; i++) {
+			passenger = output[i];
+			printf("%d.\n", i);
+			printf("Name    : %s %s\n", passenger.Name.firstName, passenger.Name.lastName);
+			printf("ID      : %s\n", passenger.id);
+			printf("Priority: %d\n", passenger.priorityNumber);
+		}
+
+		// error message when no passengers are found.
+		if (!passengersFound) {
+			system("clear||cls");
+			printf("No passengers found with that last name.\n");
+		}
+	} while (strcmp(input, "0") != 0);
 }
 
 void loadFile()
 {
 
-}
-
-void logOut()
-{
-
-}
-
-/*
- *	personnelAuthentication checks a password file and asks the user to input a password
- *	before displaying the personnel menu.
- *	Precondition: Password abides by MAX value, .password file exists, and function is called from main menu.
- *	@return Boolean value that determines whether a login was successful or not.
- */
-bool personnelAuthentication()
-{
-	FILE *fp;
-	char password[MAX];
-	char input[MAX];
-
-	bool passwordMatch = false;
-	fp = fopen(".password", "r");
-
-	if (fp == NULL) {
-		printf("CRITICAL ERROR: No password set. Contact ITS.");
-	} else {
-		fgets(password, MAX, fp);
-
-		while (!passwordMatch && strcmp(input, "0") != 0) {
-			printf("Enter the personnel password, or 0 to cancel: ");
-			scanf("%s", input);
-
-			if (strcmp(password, input) == 0) {
-				printf("INFO: Login successful.\n");
-				passwordMatch = true;
-			} else if (strcmp(input, "0") == 0) {
-				printf("INFO: Login canceled.\n");
-			} else {
-				printf("Incorrect password entered. Try again.\n");
-			}
-		}
-	}
-
-	return passwordMatch;
-}
-
-/*
- *	personnelMenu allows personnel to access the functions available to them.
- *	Precondition: Called from main menu if personnel authentication was successful.
- *	@return None.
- */
-void personnelMenu(Bus buses[], Date date)
-{
-	char userChoice;
-	printf("Personnel Management Console\n");
-	printf("Please select a menu option below: \n");
-	printf("1. View number of passengers on trip \n");
-	printf("2. View number of passengers at drop-off point \n");
-	printf("3. View passenger information \n");
-	printf("4. Load passenger information \n");
-	printf("5. Search passenger \n");
-	printf("6. Load file \n");
-	printf("7. Log out of personnel management console \n");
-
-	do
-	{
-		scanf(" %c", &userChoice);
-		system("clear||cls");
-
-		switch (userChoice)
-		{
-		case VIEW_PASSENGERS_ON_TRIP:
-			viewPassengersOnTrip();
-			break;
-		case VIEW_PASSENGERS_AT_DROP:
-			viewPassengersAtDrop();
-			break;
-		case VIEW_PASSENGER_INFO:
-			viewPasssengerInfo();
-			break;
-		case LOAD_PASSENGER_INFO:
-			loadPassengerInfo();
-			break;
-		case SEARCH:
-			searchPassenger();
-			break;
-		case LOAD_FILE:
-			loadFile();
-			break;
-		case PERSONNEL_EXIT:
-			break;
-		default:
-			printf("Please input a number from 1 - 7.\n");
-			break;
-		}
-	} while (userChoice != PERSONNEL_EXIT);
-	
 }
