30e0e80c92bc6e4a8ac2ef6771847107ac3a9258
jayylmao <jaymichael_carlos@icloud.com>
2024-03-25 22:06:59 +0800

    feat: drop off point name is now written to file

--- file.h
+++ file.h
@@ -1,9 +1,44 @@
-#define TRIP_NUMBER 0
-#define EMBARK_PT 1
-#define PASSENGER_NAME 2
-#define ID_NUMBER 3
-#define PRIORITY_NUMBER 4
-#define DROP_OFF_PT 5
+/*
+ *  setDropOffPt returns the name of a drop-off point given its integer representation in the program.
+ *  Precondition: Valid drop-off point integer given.
+ *  @param dropOffPt Integer representation of drop-off point in program
+ *  @param dropOffString Character array to save name of drop-off point to.
+ *  @return None.
+ */
+void setDropOffPt(int dropOffPt, char *dropOffString)
+{
+    switch (dropOffPt)
+    {
+    case 1:
+        strcpy(dropOffString, "Mamplasan Toll Exit");
+        break;
+    case 2:
+        strcpy(dropOffString, "San Jose Village");
+        break;
+    case 3:
+    case 5:
+        strcpy(dropOffString, "Milagros Del Rosario (MRR) Building - East Canopy");
+        break;
+    case 4:
+        strcpy(dropOffString, "Laguna Blvd. Guard House");
+        break;
+    case 6:
+        strcpy(dropOffString, "Petron Gasoline Station along Gil Puyat Avenue");
+        break;
+    case 7:
+    case 11:
+        strcpy(dropOffString, "Gate 4: Gokongwei Gate");
+        break;
+    case 8:
+    case 12:
+        strcpy(dropOffString, "Gate 2: North Gate (HSSH)");
+        break;
+    case 9:
+    case 13:
+        strcpy(dropOffString, "Gate 1: South Gate (LS Building Entrance)");
+        break;
+    }
+}
 
 /*
  *  writeFile takes all the trips in the array and writes all passenger and trip info
@@ -22,6 +57,7 @@ void writeFile(Trip *trips, int nTrips, Date date)
     Passenger passenger;
 
     char embarkPoint[60];
+    char dropOffPoint[MAX];
 
 	char destPath[MAX];
     snprintf(destPath, sizeof(destPath), "./trips/%02d-%02d-%04d.txt", date.date, date.month, date.year);
@@ -41,23 +77,20 @@ void writeFile(Trip *trips, int nTrips, Date date)
         
         if (passengerCount > 0) {
             fprintf(fp, "%d\n", trips[i].tripNumber);
+            setEmbarkName(trips[i].embarkPt, embarkPoint);
+            fprintf(fp, "%s\n", embarkPoint);
         }
 
         for (j = 0; j < passengerCount; j++) {
             passenger = trips[i].passengers[j];
-            setEmbarkName(passenger.tripNumber, embarkPoint);
-
-            // this only gets prints for 1st passenger as per MP specs.
-            if (j == 0) {
-                fprintf(fp, "%s\n", embarkPoint);
-            }
 
             // write passenger data to file.
             fprintf(fp, "%s %s\n", passenger.Name.firstName, passenger.Name.lastName);
             fprintf(fp, "%s\n", passenger.id);
             fprintf(fp, "%d\n", passenger.priorityNumber);
-            
-            fprintf(fp, "%d\n", passenger.dropOffPt);
+
+            setDropOffPt(passenger.dropOffPt, dropOffPoint);
+            fprintf(fp, "%s\n", dropOffPoint);
             
             // add extra newline for all other passengers as per MP specs.
             fprintf(fp, "\n");
@@ -68,14 +101,20 @@ void writeFile(Trip *trips, int nTrips, Date date)
 }
 
 /*
- *  readTrips reads all trips from a file of a given date.
+ *  readTrips reads all trips from a file of a given date for viewing.
+ *  Precondition: Valid date format given.
+ *  @param date Date to base file name from.
+ *  @return Success indicator.
  */
-void readTrips(Trip trips[], Date date)
+int readTrips(Trip trips[], Date date)
 {
     FILE *fp;
     
     // number of passengers
     int passNum = 0;
+
+    // trip index to save current data to.
+    int tripNum = -1;
 
     // track which line is being read.
     int lineNum = 0;
@@ -87,10 +126,39 @@ void readTrips(Trip trips[], Date date)
     snprintf(sourcePath, sizeof(sourcePath), "./trips/%02d-%02d-%02d.txt", date.date, date.month, date.year);
     fp = fopen(sourcePath, "r");
 
-    passNum++;
-    lineNum++;
+    if (fp == NULL) {
+        printf("[*]: Could not find file %02d-%02d-%04d.txt in directory ./trips/.\n", date.date, date.month, date.year);
+        return 0;
+    }
+    
+    while (!feof(fp)) {
+        fscanf(fp, "%s", buffer);
+
+        // check if buffer in integer form is a trip number and reset 
+        if (atoi(buffer) > 101 && atoi(buffer) < 160) {
+            tripNum++;
+            trips[tripNum].tripNumber = atoi(buffer);
+            lineNum = 0;
+
+            fscanf(fp, "%s", buffer);
+
+        }
+
+        switch (lineNum)
+        {
+        case 1:
+            passNum++;
+            break;
+        
+        default:
+            break;
+        }
+    }
+
+    printf("%d", passNum); // temp
 
     fclose(fp);
+    return 1;
 }
 
 // void readTrips(Trip *trips, Date date)
